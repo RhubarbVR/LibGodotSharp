7aea3e493c813859eb789036e9ada4bdbbeee495
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2023-02-18 12:02:34 -0500

    Update MainGodotGame.cs

--- TestLibGodotSharp/MainGodotGame.cs
+++ TestLibGodotSharp/MainGodotGame.cs
@@ -7,32 +7,38 @@ public class MainGodotGame
 {
     public static HelloFromLibGodot AddCube(Vector3 pos)
     {
-        var meshRender = new HelloFromLibGodot();
-        meshRender.Position = pos;
+        var meshRender = new HelloFromLibGodot
+        {
+            Position = pos
+        };
         return meshRender;
     }
 
     public static void LoadScene(SceneTree scene)
     {
         var newNode = new Node3D();
-        var cam = new Camera3D();
-        cam.Current = true;
-        cam.Position = new Vector3(0, 0, 2);
+        var cam = new Camera3D
+        {
+            Current = true,
+            Position = new Vector3(0, 0, 2)
+        };
         newNode.AddChild(cam);
         newNode.AddChild(AddCube(new Vector3(1, 1, 1)));
         newNode.AddChild(AddCube(new Vector3(-1, -1, -1)));
         newNode.AddChild(AddCube(new Vector3(0, 1, 1)));
         var e = new Label();
         e.SetAnchorsPreset(Control.LayoutPreset.FullRect);
-        var script = new GDScript();
-        script.SourceCode =
+        var script = new GDScript
+        {
+            SourceCode =
 @"
 extends Label
 
 func _ready():
 	print(get_tree().root.get_class())
 
-";
+"
+        };
         script.Reload();
         newNode.AddChild(e);
         scene.Root.AddChild(newNode);
